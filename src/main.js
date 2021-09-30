f895581ce99466def0eebae798b328f40e28e1b3
jinxb <1659340302@qq.com>
2021-10-01 01:22:36 +0800

    learn vite

--- src/main.js
+++ src/main.js
@@ -1,12 +1,33 @@
 import { sum } from './util/math.js'
 import _ from 'lodash-es';
+
+// 对css的处理
 import "./css/base.css"
 import "./css/style.less"
 
-import { dateFormat } from "../ts/format"
+// 对ts的处理
+import { dateFormat } from "./ts/format"
+
+// 对图片的处理
+import Headimg from './img/头像.jpeg'
+
+
+// 对vue的处理
+import Vue from 'vue'
+import VueApp from './vue/App.vue'
 
 console.log("hello vite");
 console.log(sum(20, 30));
 console.log(_.join(["abc", "jinxb"]));
 
 console.log(dateFormat("abc"));
+
+// img原生
+const imgEl = document.createElement('img');
+imgEl.src = Headimg;
+document.body.appendChild(imgEl);
+
+// vue 的实例
+new Vue({
+  render: h => h(VueApp)
+}).$mount("#app")
---
+++ src/vue/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2 class="title">{{ message }}</h2>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return {
+      message: 'hello Vue'
+    }
+  }
+ }
+</script>
+
+<style scoped>
+</style>
